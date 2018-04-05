f66ff13d2e2e6a3d8772b485f27862cd9502cb03
Nichole <nicholesvibrantlife@gmail.com>
2018-04-04 19:49:38 -0700

    added styling to grid. initial grid is present

--- README.md
+++ README.md
@@ -33,6 +33,7 @@ Add additional notes about how to deploy this on a live system
 After conducting research from the resources below. I found my solution: 
 
 * [Flexbox Documentation](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox)
+* [Responsive Tables with CSS](https://wisdmlabs.com/blog/responsive-tables-using-css-div-tag/)
 
 
 
--- minesweeper.js
+++ minesweeper.js
@@ -1,21 +1,46 @@
+$(document).ready(function() {
 
+	$('#startGame').on('click', function() {
+		
+	})
+})
+	var lastClicked;
 
-var initialize = function() {
-	let width = 10;
-	let height = 10;
-	let grid = [];
+	var initialize = grid(10, 10, function(el, row, col, i) {
+		console.log('in initialize');
+		console.log('clicked on element ', el);
+		console.log('clicked on row ', row);
+		console.log('clicked on col ', col);
+		console.log('clicked item ', i);
 
-	for (let i = 0; i <= height; i++) {
-		$('.game').append("<div class='row'>");
-		grid.push([]);
-		for (let j = 0; j <= width; j++) {
-			$('.game').append("<div clas='cell'><p>" + j +"</p></div>");
-			grid[i].push(0)
+		el.className = 'clicked';
+		if(lastClicked) {
+			lastClicked.className='';
+			lastClicked = el;
+		}
+	});
+
+	function grid(row, col, callBack) {
+	// debugger;
+	var i = 0;
+	let gameGrid = $('.game');
+	for (let r = 0; r < row; ++r) {
+		let gridRow = $("<div class='row'>").appendTo(gameGrid);
+
+		for (let c = 0; c < col; ++c) {
+			let cell = $("<div class='cell'>").appendTo(gridRow);
+			cell.html("<p>" + ++i + "</p>");
+
+			cell.click(function(el, r, c, i) {
+				return function() {
+					callBack(el, r, c, i);
+				}
+			}(cell, r, c, i), false)
 		}
 	}
-
-
-	console.log(grid)
-		
+	return grid;
 }
+
+
+
 
